3a13d5fdd85bc2a8f843c356f82b66b7c24bfab8
“muddyrain” <qiu3291002845@gmail.com>
2023-01-05 13:57:54 +0800

    feat: 添加 hot reload

--- nestjs-demo/src/main.ts
+++ nestjs-demo/src/main.ts
@@ -5,5 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
