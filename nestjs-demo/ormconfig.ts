9105624bc22678675754ba4d6e74cf017993b327
‘muddyrain’ <qiu3291002845@gmail.com>
2023-06-17 15:38:28 +0800

    feat: 修改ormconfig到root目录

---
+++ nestjs-demo/ormconfig.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Logs } from 'src/logs/logs.entity';
+import { Roles } from 'src/roles/roles.entity';
+import { Profile } from 'src/user/profile.entity';
+import { User } from 'src/user/user.entity';
+
+export default {
+  type: 'mysql',
+  host: '127.0.0.1',
+  port: 3306,
+  username: 'root',
+  password: 'mm001030',
+  database: 'nest-study',
+  entities: [User, Profile, Logs, Roles],
+  // 同步本地 schema 与 数据库
+  synchronize: true,
+  // logging: process.env.NODE_ENV === 'development',
+  logging: ['error'],
+  retryDelay: 5000,
+  retryAttempts: Infinity,
+} as TypeOrmModuleOptions;
--- nestjs-demo/src/app.module.ts
+++ nestjs-demo/src/app.module.ts
@@ -11,6 +11,7 @@ import { Profile } from './user/profile.entity';
 import { Logs } from './logs/logs.entity';
 import { Roles } from './roles/roles.entity';
 import { LogsModule } from './logs/logs.module';
+import ormconfig from 'ormconfig';
 
 const envFilePath = `.env.${process.env.NODE_ENV}`;
 @Global()
@@ -30,27 +31,7 @@ const envFilePath = `.env.${process.env.NODE_ENV}`;
         DB_TYPE: Joi.string().valid('mysql'),
       }),
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory(configService: ConfigService) {
-        return {
-          type: configService.get(ConfigEnum.DB),
-          host: configService.get(ConfigEnum.DB_HOST),
-          port: configService.get(ConfigEnum.DB_PORT),
-          username: configService.get(ConfigEnum.DB_USERNAME),
-          password: configService.get(ConfigEnum.DB_PASSWORD),
-          database: configService.get(ConfigEnum.DB_DATABASE),
-          entities: [User, Profile, Logs, Roles],
-          // 同步本地 schema 与 数据库
-          synchronize: configService.get(ConfigEnum.DB_SYNC),
-          // logging: process.env.NODE_ENV === 'development',
-          logging: ['error'],
-          retryDelay: 5000,
-          retryAttempts: Infinity,
-        } as TypeOrmModuleOptions;
-      },
-    }),
+    TypeOrmModule.forRoot(ormconfig),
     UserModule,
     RangeModule,
     LogsModule,
